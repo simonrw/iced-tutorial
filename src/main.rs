a37b12d16b686090c73680f7804a37c5fb0e15ec
Simon Walker <s.r.walker101@googlemail.com>
2023-11-01 13:43:58 +0000

    Add basic text editing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
-use iced::{widget::text, Element, Sandbox, Settings};
+use iced::{widget::text_editor, Element, Sandbox, Settings};
 
 fn main() -> iced::Result {
     Editor::run(Settings::default())
 }
 
-struct Editor;
+struct Editor {
+    content: text_editor::Content,
+}
 
-#[derive(Debug)]
-enum Message {}
+#[derive(Debug, Clone)]
+enum Message {
+    EditText(text_editor::Action),
+}
 
 impl Sandbox for Editor {
     type Message = Message;
 
     fn new() -> Self {
-        Self
+        Self {
+            content: text_editor::Content::new(),
+        }
     }
 
     fn title(&self) -> String {
@@ -21,10 +27,12 @@ impl Sandbox for Editor {
     }
 
     fn update(&mut self, message: Message) {
-        match message {}
+        match message {
+            Message::EditText(action) => self.content.edit(action),
+        }
     }
 
     fn view(&self) -> Element<'_, Message> {
-        text("Hello world").into()
+        text_editor(&self.content).on_edit(Message::EditText).into()
     }
 }
