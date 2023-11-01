cbf44e164a5f3170da59d139e448ddce87e2d873
Simon Walker <s.r.walker101@googlemail.com>
2023-11-01 16:57:45 +0000

    Add new button

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,7 @@ enum Message {
     EditText(text_editor::Action),
     FileOpened(Result<(PathBuf, Arc<String>), Error>),
     Open,
+    New,
 }
 
 impl Application for Editor {
@@ -50,35 +51,54 @@ impl Application for Editor {
 
     fn update(&mut self, message: Message) -> Command<Message> {
         match message {
-            Message::EditText(action) => self.content.edit(action),
+            Message::EditText(action) => {
+                self.content.edit(action);
+                Command::none()
+            }
             Message::FileOpened(Ok((path, content))) => {
                 self.path = Some(path);
                 self.content = text_editor::Content::with(&content);
                 self.error = None;
+                Command::none()
             }
-            Message::FileOpened(Err(e)) => self.error = Some(e),
-            Message::Open => return Command::perform(pick_file(), Message::FileOpened),
+            Message::FileOpened(Err(e)) => {
+                self.error = Some(e);
+                Command::none()
+            }
+            Message::Open => Command::perform(pick_file(), Message::FileOpened),
+            Message::New => {
+                self.path = None;
+                self.content = text_editor::Content::new();
+                Command::none()
+            }
         }
-
-        Command::none()
     }
 
     fn view(&self) -> Element<'_, Message> {
         let input = text_editor(&self.content).on_edit(Message::EditText);
 
-        let file_path = match self.path.as_deref().and_then(Path::to_str) {
-            Some(path) => text(path).size(14),
-            None => text(""),
+        let status_bar = {
+            let status = if let Some(Error::IO(error)) = self.error.as_ref() {
+                text(error.to_string())
+            } else {
+                match self.path.as_deref().and_then(Path::to_str) {
+                    Some(path) => text(path).size(14),
+                    None => text("New file"),
+                }
+            };
+
+            let position = {
+                let (line, column) = self.content.cursor_position();
+                text(format!("{}:{}", line + 1, column + 1))
+            };
+
+            row![status, horizontal_space(Length::Fill), position]
         };
 
-        let position = {
-            let (line, column) = self.content.cursor_position();
-            text(format!("{}:{}", line + 1, column + 1))
-        };
-
-        let status_bar = row![file_path, horizontal_space(Length::Fill), position];
-
-        let controls = row![button("Open").on_press(Message::Open)];
+        let controls = row![
+            button("New").on_press(Message::New),
+            button("Open").on_press(Message::Open)
+        ];
 
         container(column![controls, input, status_bar].spacing(10))
             .padding(10)
